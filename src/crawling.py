c2895aef95bd121456a60036c4ff451705197df3
Jungseung Yang <xyloon@gmail.com>
2019-01-16 17:12:25 +0900

    Implementing parser and option chooser

--- src/crawling.py
+++ src/crawling.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
+import re
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+title_pattern = re.compile('([\s\S]+)\s+(\d+)\s*화?')
+page_pattern = re.compile('page-(\d+)')
+remove_hwa_pattern = re.compile('([\d.\-]+)\s*화?')
+
+def get_title_parsed(string):
+    processed =  [[y.strip() for y in x] for x in title_pattern.findall(string)]
+    return processed[0] if processed else None
+
+def get_page_parsed(string):
+    processed = page_pattern.findall(string)
+    return int(processed[0]) if processed else None
+
+def get_remove_hwa(string):
+    processed = remove_hwa_pattern.findall(string)
+    return processed[0] if processed else None
+
 
 
 class Browser:
@@ -16,8 +35,7 @@ class Browser:
                                        options=options)
         self.driver.implicitly_wait(3)
 
-    def readPage(self, page_id='427271'):
-        self.driver.get("https://mangashow.me/bbs/board.php?bo_table=msm_manga&wr_id=" + page_id)
+    def __page_parse(self):
         html_page = self.driver.page_source
         if html_page:
             images_elem = self.driver.find_elements_by_xpath(
@@ -28,13 +46,21 @@ class Browser:
                 '//*[@id="thema_wrapper"]/div[2]/div/div/div[1]/div[2]/div[1]/div[1]/div/select')
             pages_elem = select_elem.find_elements_by_tag_name('option')
             images_detailed = [
-                (image.get_attribute('class'), image.get_attribute('src')) for image in images_elem]
+                (get_page_parsed(image.get_attribute('class')), image.get_attribute('src')) for image in images_elem]
             title = title_elem.text
-            pages = [(page.get_attribute('value'), page.text) for page in pages_elem]
+            pages = [(page.get_attribute('value'), get_remove_hwa(page.text.strip())) for page in pages_elem]
+            self.select_elem = Select(select_elem)
             return {
                 "images": images_detailed,
-                "title": title,  # ???? ???? ???? \d+화
-                "select_elem": select_elem,
+                "title": get_title_parsed(title),  # ???? ???? ???? \d+화
                 "page_info": pages
             }
         raise Exception("Read Problem")
+
+    def readPage(self, page_id='427271'):
+        self.driver.get("https://mangashow.me/bbs/board.php?bo_table=msm_manga&wr_id=" + page_id)
+        return self.__page_parse()
+
+    def choosePage(self, page_id):
+        self.select_elem.select_by_value(page_id)
+        return self.__page_parse()
--- test/test_crawling.py
+++ test/test_crawling.py
@@ -1,5 +1,5 @@
 from unittest import TestCase, main
-from src.crawling import Browser
+from src.crawling import Browser, get_title_parsed, get_page_parsed, get_remove_hwa
 
 
 class TestCrawling(TestCase):
@@ -8,9 +8,52 @@ class TestCrawling(TestCase):
     def tearDown(self):
         pass
     
-    def test_db_session_close(self):
+    def test_browser(self):
         browser = Browser()
         self.assertEqual(35, len(browser.readPage()['images']))
+
+    def test_browser_choose_option(self):
+        browser = Browser()
+        readen = browser.readPage(page_id='427271')
+        first_page_info = [one_page_info for one_page_info in readen['page_info'] if one_page_info[1] == '1'][0]
+        readen2 = browser.choosePage(first_page_info[0])
+        self.assertEqual(39, len(readen2['images']))
+
+    def test_print_result(self):
+        browser = Browser()
+        print(browser.readPage())
+
+
+
+    def test_get_title(self):
+        self.assertEqual(
+            ['뭔가 다른 이야기', '3'],
+            get_title_parsed("뭔가 다른 이야기 3화")
+        )
+
+    def test_get_page_parsed_0(self):
+        self.assertEqual(0, get_page_parsed('page-0'))
+
+    def test_get_page_parsed_32(self):
+        self.assertEqual(32, get_page_parsed('page-32'))
+
+    def test_getpage_parsed_none(self):
+        self.assertEqual(None, get_page_parsed('page-'))
+
+    def test_get_remove_hwa_13(self):
+        self.assertEqual('13', get_remove_hwa("13화"))
+
+    def test_get_remove_hwa_number_space(self):
+        self.assertEqual('13', get_remove_hwa("13     화"))
+
+    def test_get_remove_hwa_dot(self):
+        self.assertEqual('13.5', get_remove_hwa("13.5     화"))
+
+    def test_get_remove_hwa_dash(self):
+        self.assertEqual('13-5', get_remove_hwa(" 13-5     화"))
+
+    def test_get_remove_hwa_none(self):
+        self.assertEqual(None, get_remove_hwa("화"))
 
 if __name__ == '__main__':
     main()
